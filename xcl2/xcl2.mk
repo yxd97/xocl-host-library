a55684d54c24f2b7f224fe1a117941846a89265c
Yixiao Du <yd383@cornell.edu>
2024-05-01 16:24:51 -0400

    deprecate old examples

--- xcl2/xcl2.mk
+++ xcl2/xcl2.mk
@@ -1,4 +1,4 @@
-xcl2_SRCS:=${XOCL_HOST_LIB}/xcl2/xcl2.cpp
-xcl2_HDRS:=${XOCL_HOST_LIB}/xcl2/xcl2.hpp
+xcl2_SRCS:=${REPO_ROOT}/xcl2/xcl2.cpp
+xcl2_HDRS:=${REPO_ROOT}/xcl2/xcl2.hpp
 
-xcl2_CXXFLAGS:=-I${XOCL_HOST_LIB}/xcl2
+xcl2_CXXFLAGS:=-I${REPO_ROOT}/xcl2
